f1647c259ea47057de03cfea84f2a313f88a8100
hansalbertlianto <lianto.albert28@gmail.com>
2018-10-01 01:33:19 +0800

    functioning user login

--- login/views.py
+++ login/views.py
@@ -6,19 +6,40 @@ from django.contrib.auth import authenticate
 # Create your views here.
 
 def index(request):
+
+    loginSuccess = True
+
     if request.method == 'POST':
         userUsername = request.POST.get('username')
         userPassword = request.POST.get('password')
+        userConfirmPassword = request.POST.get('confirmPassword')
 
-        user = authenticate(username=userUsername, password=userPassword)
+        # Handle new signup from signup page
+        if userConfirmPassword is not None:
 
-        print(user)
+            # Check if username already exists. Then check if confirm password matches with password.
+            if User.objects.filter(username=userUsername).exists():
+                return render(request, 'login/signup.html', {'usernameTaken': True})
+            else:
+                if userPassword != userConfirmPassword:
+                    return render(request, 'login/signup.html', {'passwordsDontMatch': True})
+                user = User.objects.create_user(userUsername, None, userPassword)
+                user.save()
+                return render(request, 'login/success.html') # change this.
 
-        if user is not None:
-            print("hello")
+        # Find out if user credentials are correct or not.
+        credentialsCorrect = authenticate(username=userUsername, password=userPassword)
+
+        loginSuccess = (user is not None)
+
+        # If user credentials are correct, load home page. Otherwise, return a 
+        if credentialsCorrect:
             # Redirect to a success page.
-            return render(request, 'login/success.html')
+            return render(request, 'login/success.html') # change this.
         else:
-            return render(request, 'login/login.html', )
+            return render(request, 'login/login.html', {'loginSuccess': loginSuccess})
 
     return render(request, 'login/login.html')
+
+def signup(request):
+    return render(request, 'login/signup.html')

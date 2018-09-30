a8ee0d2506e399f79ff4fa5a0727f7a430a68d22
hansalbertlianto <lianto.albert28@gmail.com>
2018-09-30 09:55:37 +0800

    login screen functioning

--- login/views.py
+++ login/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 # Create your views here.
 
 def index(request):
-
     if request.method == 'POST':
-        print(request.POST)
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+        userUsername = request.POST.get('username')
+        userPassword = request.POST.get('password')
 
-        print(username)
-        print(password)
+        user = authenticate(username=userUsername, password=userPassword)
+
+        print(user)
+
+        if user is not None:
+            print("hello")
+            # Redirect to a success page.
+            return render(request, 'login/success.html')
+        else:
+            return render(request, 'login/login.html', )
 
     return render(request, 'login/login.html')
